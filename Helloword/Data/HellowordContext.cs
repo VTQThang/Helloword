79bdfc8a56374178af0df4e8d11866e84eb22f24
ASUS <ASUS@DESKTOP-BSRBTU7>
2024-08-18 16:51:56 +0630

    Add project files.

---
+++ Helloword/Data/HellowordContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace Helloword.Data
+{
+    public class HellowordContext : DbContext
+    {
+        public HellowordContext (DbContextOptions<HellowordContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
